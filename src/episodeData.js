d949572b2e684cb77c6858ed51205289c20bdc7c
Ann Dai <gofarann@gmail.com>
2017-07-05 11:21:35 -0700

    added hardcoded data to index file

---
+++ src/episodeData.js
@@ -0,0 +1,4 @@
+var data = [
+  { number: 2, title: 'Small Scale Sin', description: 'Small-scale stories on the nature of small-scale sin.', date: "1995-11-24" },
+  { number: 3, title: 'Poultry Slam 1995', description: 'Stories decrying the wonders of turkeys, chickens, and other fowl.', date: "1995-12-01" },
+];
--- src/index.js
+++ src/index.js
@@ -7,9 +7,26 @@ exports.handler = function(event, context, callback){
     alexa.execute();
 };
 
+// =====================================================================================================
+// --------------------------------- Section 1. Data and Text strings  ---------------------------------
+// =====================================================================================================
+var skillName = "Alexa This American Life Lookup";
+
+var WELCOME_MESSAGE = "Welcome to the This American Life episode lookup skill.";
+
+var data = [
+  { number: 2, title: 'Small Scale Sin', description: 'Small-scale stories on the nature of small-scale sin.', date: "1995-11-24" },
+  { number: 3, title: 'Poultry Slam 1995', description: 'Stories decrying the wonders of turkeys, chickens, and other fowl.', date: "1995-12-01" },
+];
+
+
+// =====================================================================================================
+// --------------------------------- Section 2. Handlers  ---------------------------------
+// =====================================================================================================
+
 var handlers = {
     'LaunchRequest': function () {
-         this.emit('SayHello');
+         this.emit(':ask', WELCOME_MESSAGE + getGenericHelpMessage(data));
      },
 
     'SearchByEpisodeNumberIntent': function () {
@@ -17,3 +34,19 @@ var handlers = {
     }
 
 };
+
+// =====================================================================================================
+// --------------------------------- Section 3. generate messages  ---------------------------------
+// =====================================================================================================
+function getGenericHelpMessage(data){
+  var sentences = ["ask - play episode" + getRandomEpisodeNumber(1, 500)];
+  return "You can " + sentences;
+}
+
+// =====================================================================================================
+// ------------------------------------ Section 4. Helper Functions  -----------------------------------
+// =====================================================================================================
+
+function getRandomEpisodeNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
